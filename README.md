14617b6c5cfbec1899dc2e0235ab86e50ee22a45
Drashtee <drashteeparmar@gmail.com>
2021-12-30 10:49:30 -0500

    modified dates now appear as numbers

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # new_weather_dashboard
+## Deployment
+Two options are provided the first one is link and second one thats click me that will redirect to the page
+- Application deployed to [Github](https://github.com/) : 
+  - https://github.com/dparmar32/new_weather_dashboard
+  - [Click Here](https://github.com/dparmar32/new_weather_dashboard)
+- Application live GitHub URL submitted link : 
+  - https://dparmar32.github.io/new_weather_dashboard/
+  - [Click Here](https://dparmar32.github.io/new_weather_dashboard/)
+
 ## References
 - [Moment](https://momentjs.com/?utm_source=cdnjs&utm_medium=cdnjs_link&utm_campaign=cdnjs_library)
 - [Icons](https://icons.getbootstrap.com/)
 - [Bootstrap](https://getbootstrap.com/docs/5.1/getting-started/introduction/)
+- [Button Generator](https://css3buttongenerator.com/)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,27 @@
 /* Todays Data */
 // var t_date = moment().toString();
-var t_date = moment().format('MMMM Do YYYY, h:mm:ss a');
+var t_date = moment().format('L');
 document.getElementById("todaysdate").innerHTML = t_date;
 console.log(t_date);
 
 /* Five day forecast */
 /* Day 1 */
-var day1 = moment().add(1, 'days').format('MMMM Do YYYY');
+var day1 = moment().add(1, 'days').format('L');
 document.getElementById("firstDay").innerHTML= day1;
 console.log(day1);
 /* Day 2 */
-var day2 = moment().add(2, 'days').format('MMMM Do YYYY');
+var day2 = moment().add(2, 'days').format('L');
 document.getElementById("secondDay").innerHTML= day2;
 console.log(day2);
 /* Day 3 */
-var day3 = moment().add(3, 'days').format('MMMM Do YYYY');
+var day3 = moment().add(3, 'days').format('L');
 document.getElementById("thirdDay").innerHTML= day3;
 console.log(day3);
 /* Day 4 */
-var day4 = moment().add(4, 'days').format('MMMM Do YYYY');
+var day4 = moment().add(4, 'days').format('L');
 document.getElementById("forthDay").innerHTML= day4;
 console.log(day4);
 /* Day 5 */
-var day5 = moment().add(5, 'days').format('MMMM Do YYYY');
+var day5 = moment().add(5, 'days').format('L');
 document.getElementById("fifthDay").innerHTML= day5;
 console.log(day5);
