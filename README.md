f30a606d866a276b264f3b9eb3445972c0bd8934
Drashtee <drashteeparmar@gmail.com>
2022-01-02 11:05:14 -0500

    Enable google autocomplete features

--- README.md
+++ README.md
@@ -1,15 +1,21 @@
 # new_weather_dashboard
+
 ## Deployment
+
 Two options are provided the first one is link and second one thats click me that will redirect to the page
-- Application deployed to [Github](https://github.com/) : 
+
+- Application deployed to [Github](https://github.com/) :
   - https://github.com/dparmar32/new_weather_dashboard
   - [Click Here](https://github.com/dparmar32/new_weather_dashboard)
-- Application live GitHub URL submitted link : 
+- Application live GitHub URL submitted link :
   - https://dparmar32.github.io/new_weather_dashboard/
   - [Click Here](https://dparmar32.github.io/new_weather_dashboard/)
 
 ## References
+
 - [Moment](https://momentjs.com/?utm_source=cdnjs&utm_medium=cdnjs_link&utm_campaign=cdnjs_library)
 - [Icons](https://icons.getbootstrap.com/)
 - [Bootstrap](https://getbootstrap.com/docs/5.1/getting-started/introduction/)
 - [Button Generator](https://css3buttongenerator.com/)
+- [dashboard reference](https://pixinvent.com/modern-admin-clean-bootstrap-4-dashboard-html-template/html/ltr/horizontal-menu-template/card-weather.html)
+- [AutoComplete Google API](https://developers.google.com/maps/documentation/javascript/examples/places-placeid-finder)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,64 @@
-/* Todays Data */
+/* Global variable */
+
+const weatherApiKey = "ebd153ae1727f03c10d93ef05e14e355";
+const weatherApiUrl = "api.openweathermap.org/data/2.5/";
+const unit = "metric";
+// const searchHistory = [];
+const weatherIconUrl = "openweathermap.org";
+
+/* Dom element reference */
+
+var searchBtnEl = document.querySelector("search-btn");
+var cityNameHistoryEl = document.getElementById("clear-history-btn");
+var cityNameListEl = document.getElementById("city-list");
+/* ------------------------------------------------------------------  */
+/* Google API autocomplete: enable Maps JavaScript API and Places API  */
+/* ------------------------------------------------------------------- */
+
+function initAutocomplete() {
+  // Create the search box and link it to the UI element.
+  const searchTxtEl = document.getElementById("searchTextField");
+  const searchBox = new google.maps.places.SearchBox(searchTxtEl);
+  map.controls[google.maps.ControlPosition].push(searchTxtEl);
+  searchBox.addListener("places_changed", () => {
+    const places = searchBox.getPlaces();
+    if (places.length == 0) {
+      return;
+    }
+  });
+}
+
+
+/* -------------------------------------------------------  */
+/*                  Today's Data                             */
+/* -------------------------------------------------------  */
+
 // var t_date = moment().toString();
-var t_date = moment().format('L');
+var t_date = moment().format("L");
 document.getElementById("todaysdate").innerHTML = t_date;
 console.log(t_date);
 
-/* Five day forecast */
+/* -------------------------------------------------------  */
+/*                  Five day forecast                       */
+/* -------------------------------------------------------  */
+
 /* Day 1 */
-var day1 = moment().add(1, 'days').format('L');
-document.getElementById("firstDay").innerHTML= day1;
+var day1 = moment().add(1, "days").format("L");
+document.getElementById("firstDay").innerHTML = day1;
 console.log(day1);
 /* Day 2 */
-var day2 = moment().add(2, 'days').format('L');
-document.getElementById("secondDay").innerHTML= day2;
+var day2 = moment().add(2, "days").format("L");
+document.getElementById("secondDay").innerHTML = day2;
 console.log(day2);
 /* Day 3 */
-var day3 = moment().add(3, 'days').format('L');
-document.getElementById("thirdDay").innerHTML= day3;
+var day3 = moment().add(3, "days").format("L");
+document.getElementById("thirdDay").innerHTML = day3;
 console.log(day3);
 /* Day 4 */
-var day4 = moment().add(4, 'days').format('L');
-document.getElementById("forthDay").innerHTML= day4;
+var day4 = moment().add(4, "days").format("L");
+document.getElementById("forthDay").innerHTML = day4;
 console.log(day4);
 /* Day 5 */
-var day5 = moment().add(5, 'days').format('L');
-document.getElementById("fifthDay").innerHTML= day5;
+var day5 = moment().add(5, "days").format("L");
+document.getElementById("fifthDay").innerHTML = day5;
 console.log(day5);
