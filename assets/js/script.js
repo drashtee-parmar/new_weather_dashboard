c8e786e83ffd44f6679316c180acd00f16959c66
Drashtee <drashteeparmar@gmail.com>
2022-01-11 16:13:18 -0500

    Todays weather retrieving data and icon works

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,73 +1,111 @@
-/* Global variable */
 
 var weatherApiKey = "ebd153ae1727f03c10d93ef05e14e355";
-var weatherApiUrl = "https://api.openweathermap.org/";
 var unit = "imperial";
 var weatherIconUrl = "https://openweathermap.org";
-// " http://openweathermap.org/img/wn/10d@2x.png"
+
+var inputTxtEl = document.querySelector("#searchTextField");
+var searchBtnEl = document.querySelector("#search-btn");
+var clearBtnEl = document.querySelector("#clear-search-history");
+var searchHistoryEl = document.querySelector("#search_history");
+var searchHistory = [];
+var cityFormEl = document.querySelector("#city-form");
+var searchedCityEl = document.querySelector("#searched-city");
+var pastCitySearchBtnEl = document.querySelector("#buttons");
+
+var currentDtEl = document.querySelector("#current-date");
+var currentIconEl = document.querySelector("#current-icon");
+var currentTempEl = document.querySelector("#currentTemp");
+var currentHumidEl = document.querySelector("#currentHumid");
+var currentWindEl = document.querySelector("#currentWind");
+var currentUVEl = document.querySelector("#currenUV");
+
+/* -------------------------------------------------------------------  */
+/*                              Get Current Weather                             */
+/* -------------------------------------------------------------------  */
+
+function getWeather(city) {
+  var weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + weatherApiKey + "&units=" + unit;
+  fetch(weatherUrl).then((response) => {
+    response.json().then((data) => {
+      showCurrentWeather(data, city);
+    });
+  });
+}
+
+/* -------------------------------------------------------------------  */
+/*         Show Current Weather: icon, temp, humidity, wind             */
+/*                https://openweathermap.org/current                    */
+/* -------------------------------------------------------------------  */
+function showCurrentWeather(weather, searchQuery) {
+  cityFormEl.textContent = searchQuery;
+  let weatherTempEl = weather.main.temp;
+  let weatherHumidEl = weather.main.humidity;
+  let weatherWindEl = weather.wind.speed;
+  iconEl = weather.weather[0].icon;
+  currentIconEl.src = "https://openweathermap.org/img/wn/" + iconEl + "@2x.png";
+  currentTempEl.innerHTML = weatherTempEl;
+  currentHumidEl.innerHTML = weatherHumidEl;
+  currentWindEl.innerHTML = weatherWindEl;
+}
 
 
-
-
-const inputTxtEl = document.getElementById("searchTextField");
-const searchBtnEl = document.getElementById("search-btn");
-const clearBtnEl = document.getElementById("clear-search-history");
-const searchHistoryEl = document.getElementById("search_history");
-const searchHistory = [];
 
 
 
 /* -------------------------------------------------------------------  */
-/*                              Get Weather                             */
-/* -------------------------------------------------------------------  */
-//   let weatherURL = + weatherApiUrl + "data/2.5/weather?q=" + searchTextField + "&appid=" + weatherApiKey + "&unit=" + unit;
-
-
-
-/* -------------------------------------------------------------------  */
-/* Show Weather */
+/*          handle submit event                                         */
+/*          show and save list button with history                      */
+/*          Retrive from local storage                                  */
 /* -------------------------------------------------------------------  */
 
 
-
-
-
-
-/* -------------------------------------------------------------------  */
-/*          Event listener search click event            */
-/* -------------------------------------------------------------------  */
-
-searchBtnEl.addEventListener("click", function () {
-  const lookupItem = inputTxtEl.value;
-  searchHistory.push(lookupItem);
-  localStorage.setItem("search", JSON.stringify(searchHistory));
-  showSearchHistory();
-})
-
-
-/* ------------------------------------------------------------------  */
-/*  Show History */
-/* ------------------------------------------------------------------  */
-function showSearchHistory(){
-  searchHistoryEl.innerHTML = "";
-  for(let i=0; i < searchHistory.length; i++){
-    const historyList = document.createElement("input");
-    historyList.setAttribute("class", "form-control d-block bg-white");
-    historyList.setAttribute("type", "text");
-    historyList.setAttribute("readonly", "true");
-    historyList.setAttribute("value", searchHistory[i]);
-    historyList.addEventListener("click", function(){
-      // TODO
-      // getWeather
-    })
-    searchHistoryEl.append(historyList);
+function formSubmitHandler(event) {
+  event.preventDefault();
+  let cityEl = inputTxtEl.value.trim(); // saturating the value of city
+  let btn = document.createElement("button");
+  btn.className = "searched-list btn";
+  btn.innerHTML = cityEl;
+  buttons.appendChild(btn);
+  listCity();
+  if (!searchHistory.includes(cityEl) && cityEl != "") {
+    searchHistory.push(cityEl);
+  }
+  localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+  if (cityEl) {
+    getWeather(cityEl);
+    inputTxtEl.value = "";
+  } else {
+    alert("Enter a city name to get the weather!");
   }
 }
-  showSearchHistory();
-  if(searchHistory.length > 0){
-    // TODO
-    // getWeather
+function listCity() {
+  searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
+  if (!searchHistory) {
+    searchHistory = [];
+  }
 }
+function saveSearch() {
+  for (var i = 0; i < searchHistory.length; i++) {
+    let btn = document.createElement("button");
+    btn.className = "searched-list btn";
+    btn.innerHTML = searchHistory[i];
+    buttons.appendChild(btn);
+  }
+  /* ************************************************* */
+  /* Past Search Button                                */
+  /* ************************************************* */
+  let pastListBtnEl = document.querySelectorAll(".searched-list");
+  for (var i = 0; i < pastListBtnEl.length; i++) {
+    pastListBtnEl[i].addEventListener("click", (event) => {
+      getWeather(event.target.textContent);
+    });
+  }
+}
+/* Listen to the submitted query form */
+cityFormEl.addEventListener("submit", formSubmitHandler);
+listCity();
+saveSearch();
+
 
 /* ------------------------------------------------------------------  */
 /* Google API autocomplete: enable Maps JavaScript API and Places API  */
@@ -86,15 +124,13 @@ function initAutocomplete() {
   });
 }
 
-
 /* -------------------------------------------------------  */
 /*                  Today's Data                             */
 /* -------------------------------------------------------  */
 
-// var t_date = moment().toString();
-var t_date = moment().format("L");
-document.getElementById("todaysdate").innerHTML = t_date;
-console.log(t_date);
+var currentDt = moment().format("L");
+currentDtEl.innerHTML = currentDt;
+console.log(currentDt);
 
 /* -------------------------------------------------------  */
 /*                  Five day forecast                       */
